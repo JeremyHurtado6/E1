c92dfb0c99d1dc023fa53d4eefae1213cdd34e11
JeremyHurtado6 <Jeremy.hurtado.gaspar@utelvt.edu.ec>
2022-10-01 19:57:01 +0000

    programa de compara.cpp

---
+++ compara.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  float j,a;
+  cout<<"Ingrese j  : "; cin>>j;
+  cout<<"Ingrese a  : "; cin>>a;
+  if(j==a){
+     cout<<"Son iguales"<<endl;
+  }else{
+     if(j<a){
+       cout<<j<<" es el menor que "<<a<<"\n";
+     }else{
+       cout<<a<<" es el menor que "<<j<<"\n";
+     }
+  }
+
+return (0);;
+
+
+
+
+
+
+}
